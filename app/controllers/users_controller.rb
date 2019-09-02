f75b3d679d94b57ff765a31df6fcca992a7bd8b2
Facundo <facutrademark@gmail.com>
2019-09-02 01:53:35 -0300

    Added edit form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,27 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = "Welcome to the alpha blog #{@user.username}"
+            flash[:success] = "Bienvenido #{@user.username}"
             redirect_to articles_path
         else
             render 'new'
         end
     end 
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Información actualizada con éxito."
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
 
     private
     def user_params
